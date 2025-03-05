92915f4bdccbd2afd45815006a48ade1535db5ad
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-05 16:07:14 -0500

    examples/hash-list.rs
    diff --git a/examples/hash-list.rs b/examples/hash-list.rs
    index 1cf34d3..3c3db55 100644
    --- a/examples/hash-list.rs
    +++ b/examples/hash-list.rs
    @@ -291,6 +291,7 @@ fn print_commit(commit: &Commit) {
             }
             println!();
         }
    +	println!("commit {}",commit.id());
    
         let author = commit.author();
         println!("Author: {}", author);

--- examples/hash-list.rs
+++ examples/hash-list.rs
@@ -291,6 +291,7 @@ fn print_commit(commit: &Commit) {
         }
         println!();
     }
+	println!("commit {}",commit.id());
 
     let author = commit.author();
     println!("Author: {}", author);
